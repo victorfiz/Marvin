8e932275d0bcb682767779843cff8e293f9cf897
victorfiz <32594369+victorfiz@users.noreply.github.com>
2021-10-04 14:05:16 +0100

    Initial Upload

---
+++ Marvin/script.js
@@ -0,0 +1,9 @@
+const body = document.body
+
+// const div = document.createElement("div")
+// div.innerText = "Hello, my name is Marvin"
+
+const header = document.createElement('header')
+header.innerText = "Marvin"
+
+body.append(header)
